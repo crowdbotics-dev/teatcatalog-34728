904b321037d24d3c48c4c0212921475a003ed5b0
Crowdbotics <team@crowdbotics.com>
2022-12-27 19:03:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/teatcatalog_34728/MainActivity.java
@@ -0,0 +1,15 @@
+package com.teatcatalog_34728;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "teatcatalog_34728";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "teatcatalog-34728",
+  url: "https://teatcatalog-34728.botics.co",
+  api: "https://teatcatalog-34728.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
